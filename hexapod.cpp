42f3eb144639bffb403d69701cd0d9abbb0b8cdd
Dorian <doriank29tehc@gmail.com>
2022-11-16 14:24:18 +0100

    function reorder, print change

--- hexapod.cpp
+++ hexapod.cpp
@@ -3,6 +3,7 @@
 
 /**
  * TODO:
+ *	92 deg walking bug
  *
  * Features:
  * Print end pos of leg
@@ -22,7 +23,21 @@ Keyboard *keyboard;
 leg leg1, leg2, leg3, leg4, leg5, leg6;
 
 // DEBUG
-string excludedTags[4] = {"wait", "movement", "limits", "calc"};
+// 1st bool is for empty
+bool excludedTags[5] = {true, true, true, true, true};
+
+int getTagIndex(string TAG)
+{
+	if (TAG == "wait")
+		return 1;
+	else if (TAG == "movement")
+		return 2;
+	else if (TAG == "limits")
+		return 3;
+	else if (TAG == "calc")
+		return 4;
+	return 0;
+}
 // DEBUG-END
 
 float width, height;
@@ -81,36 +96,7 @@ int getLegNum(leg *givenLeg)
 		return 5;
 	else if (givenLeg == &leg6)
 		return 6;
-}
-
-void print(string output, string TAG = "empty")
-{
-	for (int i = 0; i < 4; i++)
-		if (excludedTags[i] == TAG)
-			return;
-
-	cout << TAG + ": " << output << endl;
-}
-
-// given leg with preset vector3 l values and motor1 angle calculate the rest
-void legCalc(leg *leg)
-{
-	leg->D.x = leg->l.x - leg->s1.x;
-	leg->D.y = leg->l.y - leg->s1.y;
-	leg->D.z = leg->l.z - leg->s1.z;
-
-	leg->s2.x = leg->s1.x + joint1 * cos(leg->servo1.angle + leg->angle);
-	leg->s2.y = leg->s1.y + joint1 * sin(leg->servo1.angle + leg->angle);
-	leg->s2.z = leg->s1.z;
-
-	leg->L.x = leg->D.x + leg->s2.x;
-	leg->L.y = leg->D.y + leg->s2.y;
-	leg->L.z = leg->D.z + leg->s2.z;
-
-	float P = atan(abs(leg->L.z) / (sqrt(leg->L.x * leg->L.x + leg->L.y * leg->L.y)));
-	float R = asin((abs(leg->L.z) - abs(leg->l.z)) / joint1);
-	leg->servo2.angle = -1 * (acos((pow(joint2, 2) + pow(leg->L.length(), 2) - pow(joint3, 2)) / (2 * joint2 * leg->L.length())) - (P + R));
-	leg->servo3.angle = -1 * (PI - acos((pow(joint2, 2) + pow(joint3, 2) - pow(leg->L.length(), 2)) / (2 * joint2 * joint3)));
+	return 0;
 }
 
 void initServos()
@@ -124,6 +110,35 @@ void initServos()
 			temp_leg->servoSwitch(j).motor->setVelocity(motorSpeed);
 		}
 	}
+}
+
+// set stance of robot
+void stance(char stance)
+{
+	switch (stance)
+	{
+	case 'n': // motor1 in the middle of its allowed values
+		leg1.servo1.angle = 0;
+		leg2.servo1.angle = 0;
+		leg3.servo1.angle = 0;
+		leg4.servo1.angle = 0;
+		leg5.servo1.angle = 0;
+		leg6.servo1.angle = 0;
+		break;
+	}
+}
+
+/**
+ * CONTROL
+ */
+
+void print(string output, string TAG = "empty")
+{
+	if (excludedTags[getTagIndex(TAG) - 1])
+		return;
+
+	cout << TAG + ": " << output << endl;
+	return;
 }
 
 void readData()
@@ -150,6 +165,10 @@ void readData()
 	if (key != -1)
 		print(to_string(key), "keyboard");
 }
+
+/**
+ * DELAY
+ */
 
 void Wait(int time)
 {
@@ -200,87 +219,9 @@ void legWait(int legNum = 0)
 			servoWait(&legSwitch(tempLegNum));
 }
 
-// write calculated values to all servos of a leg
-void legWrite(leg *Leg)
-{
-	// write calculated angles to the servo motors
-	Leg->servo1.motor->setPosition(Leg->servo1.angle);
-	Leg->servo2.motor->setPosition(Leg->servo2.angle);
-	Leg->servo3.motor->setPosition(Leg->servo3.angle);
-
-	Leg->servoRadToDeg(); // change calculated angle to degrees
-	// print to serial
-	print("Leg: " + to_string(getLegNum(Leg)) + " servo1: " + to_string(Leg->servo1.angle) + " servo2: " + to_string(Leg->servo2.angle) + " servo3: " + to_string(Leg->servo3.angle), "movement");
-	Leg->servoDegToRad();
-
-	// wait for the servo to reach the calculated angle
-	if (wait)
-		legWait(getLegNum(Leg));
-}
-
-// set stance of robot
-void stance(char stance)
-{
-	switch (stance)
-	{
-	case 'n': // motor1 in the middle of its allowed values
-		leg1.servo1.angle = 0;
-		leg2.servo1.angle = 0;
-		leg3.servo1.angle = 0;
-		leg4.servo1.angle = 0;
-		leg5.servo1.angle = 0;
-		leg6.servo1.angle = 0;
-		break;
-	}
-}
-
-void legValues();
-
-void calculateM1(leg *Leg)
-{
-	int legNum = getLegNum(Leg);
-	// M1 angle fix for webots
-	if (legNum == 1 || legNum == 4)
-		Leg->servo1.angle = atan(Leg->l.y / Leg->l.x); // find the correct formula -- obsidian note
-	else if (legNum == 2 || legNum == 5)
-		Leg->servo1.angle = atan(Leg->l.y / Leg->l.x) - PI / 3; // find the correct formula -- obsidian note
-	else if (legNum == 3 || legNum == 6)
-		Leg->servo1.angle = atan(Leg->l.y / Leg->l.x) + PI / 3; // find the correct formula -- obsidian note
-}
-
-void walk()
-{
-	print("started walking", "movement");
-	bool limit = false;
-	float gaitSteps = walkingCadence / gaitStepMin;
-	while (!limit && mode == 3)
-	{
-		for (int legNum = 1; legNum <= 6; legNum++)
-		{
-			leg *local_leg = &legSwitch(legNum);
-			bool legDirection = (legNum % 2 == 1) ? true : false; // legs number 1,3,5
-
-			local_leg->l.x += -1 * pow(-1, legDirection) * pow(-1, walkingDirection) * cos(walkingAngle * DEG_TO_RAD) * walkingCadence / gaitSteps; // -1 because vector is backwards
-			local_leg->l.y += -1 * pow(-1, legDirection) * pow(-1, walkingDirection) * sin(walkingAngle * DEG_TO_RAD) * walkingCadence / gaitSteps; // -1 because vector is backwards
-			local_leg->l.z = (walkingDirection) ? height - addedHeight * legDirection : height - addedHeight * !legDirection;
-
-			calculateM1(local_leg);
-
-			print(to_string(legNum) + ": l.x=" + to_string(local_leg->l.x) + " l.y=" + to_string(local_leg->l.y) + " l.z" + to_string(local_leg->l.z), "calc");
-
-			legCalc(local_leg);
-			legWrite(local_leg);
-
-			print(to_string(abs(local_leg->l.x)) + "-" + to_string(abs(limits[legNum][0])) + "=" + to_string(abs(local_leg->l.x) - abs(limits[legNum][0])) + " >= " + to_string(walkingCadence * cos(walkingAngle) / 2), "limits");
-			print(to_string(abs(local_leg->l.y)) + "-" + to_string(abs(limits[legNum][1])) + "=" + to_string(abs(local_leg->l.x) - abs(limits[legNum][0])) + " >= " + to_string(walkingCadence * sin(walkingAngle) / 2), "limits");
-
-			if (abs(local_leg->l.x) - abs(limits[legNum][0]) >= walkingCadence * cos(walkingAngle) / 2 && abs(local_leg->l.y) - abs(limits[legNum][1]) >= walkingCadence * sin(walkingAngle) / 2)
-				limit = true;
-		}
-	}
-	walkingDirection = !walkingDirection;
-	print("ended walking", "movement");
-}
+/**
+ * CALCULATION
+ */
 
 // get vector l with values for current width, height and motor1 angle
 vector3 get_l(leg *local_leg, float motor1 = -99)
@@ -307,7 +248,85 @@ vector3 get_l(leg *local_leg, float motor1 = -99)
 	return l;
 }
 
-// TODO:
+// given leg with preset vector3 l values and motor1 angle calculate the rest
+void legCalc(leg *leg)
+{
+	print("s1.x: " + to_string(abs(leg->s1.x)) + "s1.y: " + to_string(leg->s1.y) + "s1.z: " + to_string(leg->s1.z), "calc");
+
+	leg->D.x = leg->l.x - leg->s1.x;
+	leg->D.y = leg->l.y - leg->s1.y;
+	leg->D.z = leg->l.z - leg->s1.z;
+
+	leg->s2.x = leg->s1.x + joint1 * cos(leg->servo1.angle + leg->angle);
+	leg->s2.y = leg->s1.y + joint1 * sin(leg->servo1.angle + leg->angle);
+	leg->s2.z = leg->s1.z;
+
+	leg->L.x = leg->D.x + leg->s2.x;
+	leg->L.y = leg->D.y + leg->s2.y;
+	leg->L.z = leg->D.z + leg->s2.z;
+
+	float P = atan(abs(leg->L.z) / (sqrt(leg->L.x * leg->L.x + leg->L.y * leg->L.y)));
+	float R = asin((abs(leg->L.z) - abs(leg->l.z)) / joint1);
+	leg->servo2.angle = -1 * (acos((pow(joint2, 2) + pow(leg->L.length(), 2) - pow(joint3, 2)) / (2 * joint2 * leg->L.length())) - (P + R));
+	leg->servo3.angle = -1 * (PI - acos((pow(joint2, 2) + pow(joint3, 2) - pow(leg->L.length(), 2)) / (2 * joint2 * joint3)));
+}
+
+void calculateM1(leg *Leg)
+{
+	int legNum = getLegNum(Leg);
+	// M1 angle fix for webots
+	if (legNum == 1 || legNum == 4)
+		Leg->servo1.angle = atan(Leg->l.y / Leg->l.x); // find the correct formula -- obsidian note
+	else if (legNum == 2 || legNum == 5)
+		Leg->servo1.angle = atan(Leg->l.y / Leg->l.x) - PI / 3; // find the correct formula -- obsidian note
+	else if (legNum == 3 || legNum == 6)
+		Leg->servo1.angle = atan(Leg->l.y / Leg->l.x) + PI / 3; // find the correct formula -- obsidian note
+}
+
+void legValues()
+{
+	for (int legNum = 1; legNum <= 6; legNum++)
+	{
+		leg *local_leg = &legSwitch(legNum);
+		local_leg->angle = PI / 3 * (legNum - 1);
+		local_leg->s1.x = 50 * cos(local_leg->angle);
+		local_leg->s1.y = 50 * sin(local_leg->angle);
+		local_leg->s1.z = -12.8;
+	}
+}
+
+bool checkLimits(leg *Leg)
+{
+	int legNum = getLegNum(Leg) - 1;
+	print(to_string(abs(Leg->l.x)) + "-" + to_string(abs(limits[legNum][0])) + "=" + to_string(abs(Leg->l.x) - abs(limits[legNum][0])) + " >= " + to_string(walkingCadence * cos(walkingAngle) / 2), "limits");
+	print(to_string(abs(Leg->l.y)) + "-" + to_string(abs(limits[legNum][1])) + "=" + to_string(abs(Leg->l.x) - abs(limits[legNum][0])) + " >= " + to_string(walkingCadence * sin(walkingAngle) / 2), "limits");
+
+	if (abs(Leg->l.x) - abs(limits[legNum][0]) >= walkingCadence * cos(walkingAngle) / 2 && abs(Leg->l.y) - abs(limits[legNum][1]) >= walkingCadence * sin(walkingAngle) / 2)
+		return true;
+	return false;
+}
+
+/**
+ * MOVEMENT
+ */
+
+// write calculated values to all servos of a leg
+void legWrite(leg *Leg)
+{
+	// write calculated angles to the servo motors
+	Leg->servo1.motor->setPosition(Leg->servo1.angle);
+	Leg->servo2.motor->setPosition(Leg->servo2.angle);
+	Leg->servo3.motor->setPosition(Leg->servo3.angle);
+
+	Leg->servoRadToDeg(); // change calculated angle to degrees
+	// print to serial
+	print("Leg: " + to_string(getLegNum(Leg)) + " servo1: " + to_string(Leg->servo1.angle) + " servo2: " + to_string(Leg->servo2.angle) + " servo3: " + to_string(Leg->servo3.angle), "movement");
+	Leg->servoDegToRad();
+
+	// wait for the servo to reach the calculated angle
+	if (wait)
+		legWait(getLegNum(Leg));
+}
 
 // given a pos (either relative to current zero pos(m1 = 0, width distance, height distance), or absolute to body center)
 // set values of vector l of given leg accordingly
@@ -327,10 +346,74 @@ void moveTo(leg *local_leg, vector3 pos, bool absolute = false)
 		local_leg->l.z = pos.z - zero.z;
 	}
 
+	int legNum = getLegNum(local_leg);
+	print(to_string(legNum) + ": l.x=" + to_string(local_leg->l.x) + " l.y=" + to_string(local_leg->l.y) + " l.z" + to_string(local_leg->l.z), "calc");
+
 	calculateM1(local_leg);
 	legCalc(local_leg);
 	legWrite(local_leg);
 }
+
+void walk()
+{
+	print("started walking", "movement");
+	bool limit = false;
+	float gaitSteps = walkingCadence / gaitStepMin;
+	while (!limit && mode == 3)
+	{
+		for (int legNum = 1; legNum <= 6; legNum++)
+		{
+			leg *local_leg = &legSwitch(legNum);
+			bool legDirection = (legNum % 2 == 1) ? true : false; // legs number 1,3,5
+
+			local_leg->l.x += -1 * pow(-1, legDirection) * pow(-1, walkingDirection) * cos(walkingAngle * DEG_TO_RAD) * walkingCadence / gaitSteps; // -1 because vector is backwards
+			local_leg->l.y += -1 * pow(-1, legDirection) * pow(-1, walkingDirection) * sin(walkingAngle * DEG_TO_RAD) * walkingCadence / gaitSteps; // -1 because vector is backwards
+			local_leg->l.z = (walkingDirection) ? height - addedHeight * legDirection : height - addedHeight * !legDirection;
+
+			calculateM1(local_leg);
+
+			print(to_string(legNum) + ": l.x=" + to_string(local_leg->l.x) + " l.y=" + to_string(local_leg->l.y) + " l.z" + to_string(local_leg->l.z), "calc");
+
+			legCalc(local_leg);
+			legWrite(local_leg);
+
+			if (abs(local_leg->l.x) - abs(limits[legNum][0]) >= walkingCadence * cos(walkingAngle) / 2 && abs(local_leg->l.y) - abs(limits[legNum][1]) >= walkingCadence * sin(walkingAngle) / 2)
+				limit = true;
+			// limit = checkLimits(local_leg);
+		}
+	}
+	walkingDirection = !walkingDirection;
+	print("ended walking", "movement");
+}
+
+void walk2()
+{
+	print("started walking", "movement");
+	bool limit = false;
+	float gaitSteps = walkingCadence / gaitStepMin;
+	while (!limit && mode == 3)
+	{
+		for (int legNum = 1; legNum <= 6; legNum++)
+		{
+			leg *local_leg = &legSwitch(legNum);
+			bool legDirection = (legNum % 2 == 1) ? true : false; // legs number 1,3,5
+
+			vector3 pos = {-1 * pow(-1, legDirection) * pow(-1, walkingDirection) * cos(walkingAngle * DEG_TO_RAD) * walkingCadence / gaitSteps,
+						   -1 * pow(-1, legDirection) * pow(-1, walkingDirection) * sin(walkingAngle * DEG_TO_RAD) * walkingCadence / gaitSteps,
+						   (walkingDirection) ? -addedHeight * legDirection : -addedHeight * !legDirection};
+
+			moveTo(local_leg, pos);
+
+			limit = checkLimits(local_leg);
+		}
+	}
+	walkingDirection = !walkingDirection;
+	print("ended walking", "movement");
+}
+
+/**
+ * MAIN
+ */
 
 int main(int argc, char **argv)
 {
@@ -413,7 +496,6 @@ int main(int argc, char **argv)
 				mode = 2;
 			break;
 		case 4:
-			// TODO: idk fix bro
 			vector3 pos = {30, 30, height};
 			cout << &leg1 << endl;
 			print(to_string(pos.x) + " " + to_string(pos.y) + " " + to_string(pos.z) + " ");
@@ -425,16 +507,4 @@ int main(int argc, char **argv)
 
 	delete robot;
 	return 0;
-}
-
-void legValues()
-{
-	for (int legNum = 1; legNum <= 6; legNum++)
-	{
-		leg *local_leg = &legSwitch(legNum);
-		local_leg->angle = PI / 3 * (legNum - 1);
-		local_leg->s1.x = 50 * cos(local_leg->angle);
-		local_leg->s1.y = 50 * sin(local_leg->angle);
-		local_leg->s1.z = -12.8;
-	}
 }
